08b764073b7748ba7fe65368200cdcc6d98c008c
orzogc <orzogc@163.com>
2022-10-09 18:13:10 +0800

    fix ordering files

--- main.go
+++ main.go
@@ -537,14 +537,16 @@ func initialize() (e error) {
 		}
 	}
 
-	// 将cid对应文件夹设置为时间降序
-	orderBody := fmt.Sprintf("user_order=user_ptime&file_id=%d&user_asc=0&fc_mix=0", config.CID)
-	v, err := postFormJSON(orderURL, orderBody)
-	checkErr(err)
-	if !v.GetBool("state") {
-		panic(fmt.Sprintf("排序文件夹 %d 出现错误：%v", config.CID, v.GetStringBytes("error")))
-	} else if *verbose {
-		log.Printf("排序文件夹 %d 成功", config.CID)
+	if len(flag.Args()) != 0 && (*fastUpload || *upload || *multipartUpload) {
+		// 将cid对应文件夹设置为时间降序
+		orderBody := fmt.Sprintf("user_order=user_ptime&file_id=%d&user_asc=0&fc_mix=0", config.CID)
+		v, err := postFormJSON(orderURL, orderBody)
+		checkErr(err)
+		if !v.GetBool("state") {
+			panic(fmt.Sprintf("排序文件夹 %d 出现错误：%v", config.CID, v.GetStringBytes("error")))
+		} else if *verbose {
+			log.Printf("排序文件夹 %d 成功", config.CID)
+		}
 	}
 
 	ecdhCipher, err = cipher.NewEcdhCipher()
